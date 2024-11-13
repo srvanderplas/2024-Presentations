42c642f3f13c65a50394a46f57bec3df965077db
Susan Vanderplas <srvanderplas@gmail.com>
2024-11-13 11:35:48 -0600

    Add code link

---
+++ 11-fyre-graphics/code.R
@@ -0,0 +1,11 @@
+if (!"palmerpenguins" %in% installed.packages()) {
+  install.packages("palmerpenguins")
+}
+
+if (!"ggplot2" %in% installed.packages()) {
+  install.packages("ggplot2")
+}
+
+library(palmerpenguins)
+library(ggplot2)
+
