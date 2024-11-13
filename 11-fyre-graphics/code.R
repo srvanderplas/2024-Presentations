e2aa6e7408d9f4ed5a69c1f2ad31b0774039a200
Susan Vanderplas <srvanderplas@gmail.com>
2024-11-13 13:36:21 -0600

    Code as presented

--- 11-fyre-graphics/code.R
+++ 11-fyre-graphics/code.R
@@ -9,3 +9,26 @@ if (!"ggplot2" %in% installed.packages()) {
 library(palmerpenguins)
 library(ggplot2)
 
+data(penguins)
+
+# Differences between males and females in each species
+penguin_fix <- na.omit(penguins)
+ggplot(penguin_fix, aes(x = bill_length_mm, y = bill_depth_mm, color = species, 
+                        shape = sex, linetype = sex)) + 
+  geom_point(size = 5) + 
+  scale_shape_manual(values = c("male" = "M", "female" = "F"), na.value = "?") + 
+  stat_ellipse()
+
+ggplot(penguin_fix, aes(x = bill_length_mm, y = bill_depth_mm, color = species, 
+                        shape = sex, linetype = sex)) + 
+  geom_point(size = 5) + 
+  scale_shape_manual(values = c("male" = "M", "female" = "F"), na.value = "?") + 
+  stat_smooth(method = "lm")
+
+
+
+ggplot(penguin_fix, aes(x = bill_length_mm, y = bill_depth_mm, color = species)) + 
+  geom_point(size = 5) + 
+  facet_grid(~sex)
+
+ggplot(penguin_fix, aes(x = bill_length_mm, fill = species)) + geom_histogram(position = "fill")
